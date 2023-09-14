403a6dbe44f1c90330982bef14284b2dff0d327c
Nicolas Peña <103202106+Nicolasp-dev@users.noreply.github.com>
2023-09-13 19:52:51 -0500

    Custom Observable Example

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,40 +1,17 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {
-    concat,
-    fromEvent,
-    interval,
-    noop,
-    observable,
-    Observable,
-    of,
-    timer,
-    merge,
-    Subject,
-    BehaviorSubject,
-    AsyncSubject,
-    ReplaySubject
-} from 'rxjs';
-import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
-import {createHttpObservable} from '../common/util';
-
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { Observable } from "rxjs";
+import { createHttpObservable } from "../utils";
+import { map } from "rxjs/operators";
 
 @Component({
-    selector: 'about',
-    templateUrl: './about.component.html',
-    styleUrls: ['./about.component.css']
+  selector: "about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"],
 })
 export class AboutComponent implements OnInit {
+  ngOnInit() {
+    const http$ = createHttpObservable("/api/courses");
 
-    ngOnInit() {
-
-
-    }
-
-
+    const courses$ = http$.pipe(map((res) => Object.values(res["payload"])));
+  }
 }
-
-
-
-
-
-
